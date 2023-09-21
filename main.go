6e36fb1a4b3c22c22b0f55cf4c6b340efe3fc682
Raptor-wxw <296854007@qq.com>
2023-09-21 23:12:19 +0800

    v1.0.0

--- main.go
+++ main.go
@@ -5,48 +5,35 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net"
+	"net/http"
 	"strings"
+	"sync"
 	"time"
-
-	"github.com/CreditTone/aslist"
-	colorfulog "github.com/CreditTone/colorfulog"
-	"github.com/lxzan/hasaki"
 )
 
 var (
+	mu          sync.Mutex
+	proxyList   = [50]string{}
+	freeIndex   = make(chan int, 50)
 	ProxyApiUrl = "https://api.xiaoxiangdaili.com/ip/get?appKey=956913709144756224&appSecret=eTDUzAY1&cnt=1&wt=json"
-	proxyList   = aslist.NewAsList()
-	invalid     = make(chan string)
 )
+
+func init() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+}
 
 func checkProxy(proxy string) (isValid bool, timeOut float64) {
 	proxyParts := strings.Split(proxy, ":")
 
 	startTime := time.Now()
-	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%s", proxyParts[0], proxyParts[1]), time.Second*5)
+	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%s", proxyParts[0], proxyParts[1]), time.Second*1)
 
 	if err != nil {
 		return false, -1
 	}
-	defer func(conn net.Conn) {
-		err := conn.Close()
-		if err != nil {
-			colorfulog.Info(err)
-		}
-	}(conn)
-
-	_, err = conn.Write([]byte(fmt.Sprintf("GET / HTTP/1.1\r\nHost: %s\r\n\r\n", "8.8.8.8")))
-	if err != nil {
-		return false, -1
-	}
-
-	buf := make([]byte, 1024)
-	_, err = conn.Read(buf)
-	if err != nil {
-		return false, -1
-	}
-
+	defer func(conn net.Conn) { _ = conn.Close() }(conn)
 	elapsedTime := time.Since(startTime)
 	return true, elapsedTime.Seconds()
 }
@@ -58,28 +45,41 @@ func setGlobalProxyList(pl []interface{}) {
 	for _, p := range pl {
 		m := p.(map[string]interface{})
 		proxy := fmt.Sprintf("%s:%v", m["ip"], m["port"])
-		colorfulog.Infof("add proxy %v", proxy)
-		proxyList.LeftPush(proxy)
-	}
-	for proxyList.Length() > 50 {
-		colorfulog.Infof("remove proxy %v", proxyList.RightPop())
+		select {
+		case index := <-freeIndex:
+			log.Printf("add proxy %v\n", proxy)
+			proxyList[index] = proxy
+			continue
+		case <-time.NewTimer(time.Microsecond * 500).C:
+			log.Println("Proxy is full")
+			return
+		}
 	}
 }
 
-func updateUpstreamProxy() {
+func addSomeProxies() {
+	if len(freeIndex) == 0 {
+		return
+	}
+
 	defer func() {
 		if err := recover(); err != nil {
-			colorfulog.Warnf("fetchUpstreamProxy %v", err)
+			log.Printf("fetchUpstreamProxy %v\n", err)
 		}
 	}()
-	body, err := hasaki.Get(ProxyApiUrl).Send(nil).GetBody()
+	response, err := http.Get(ProxyApiUrl)
+	if err != nil {
+		return
+	}
+	defer func(Body io.ReadCloser) { _ = Body.Close() }(response.Body)
+	body, err := io.ReadAll(response.Body)
 	if err != nil {
 		return
 	}
 	var mapResult map[string]interface{}
 	err = json.Unmarshal(body, &mapResult)
 	if err != nil {
-		colorfulog.Warnf("JsonToMapDemo err: %v ", err)
+		log.Printf("JsonToMapDemo err: %v\n", err)
 		return
 	}
 	code := int(mapResult["code"].(float64))
@@ -88,85 +88,82 @@ func updateUpstreamProxy() {
 		if proxyList, ok := result.([]interface{}); ok {
 			setGlobalProxyList(proxyList)
 		} else {
-			colorfulog.Warnf("convert field %v", result)
+			log.Printf("convert field %v\n", result)
 		}
 	}
 }
 
+func getRandomProxy() string {
+	proxy := ""
+	for i := 0; i < 50 && proxy == ""; i++ {
+		proxy = proxyList[rand.Intn(50)]
+	}
+	return proxy
+}
+
 func forward(conn net.Conn, remoteAddr string) {
-	client, err := net.Dial("tcp", remoteAddr)
+	client, err := net.DialTimeout("tcp", remoteAddr, time.Second*time.Duration(1))
 	if err != nil {
 		log.Printf("Dial failed: %v", err)
-		defer func(conn net.Conn) {
-			_ = conn.Close()
-		}(conn)
+		for index := 0; index < 50; index++ {
+			if proxyList[index] == remoteAddr {
+				log.Printf("remove inactive proxy %s\n", proxyList[index])
+				mu.Lock()
+				proxyList[index] = ""
+				freeIndex <- index
+				mu.Unlock()
+			}
+		}
+		forward(conn, getRandomProxy())
 		return
 	}
 	log.Printf("Forwarding from %v to %v\n", conn.LocalAddr(), client.RemoteAddr())
 	go func() {
-		defer func(client net.Conn) {
-			_ = client.Close()
-		}(client)
-		defer func(conn net.Conn) {
-			_ = conn.Close()
-		}(conn)
+		defer func(client net.Conn) { _ = client.Close() }(client)
+		defer func(conn net.Conn) { _ = conn.Close() }(conn)
 		_, _ = io.Copy(client, conn)
 	}()
 	go func() {
-		defer func(client net.Conn) {
-			_ = client.Close()
-		}(client)
-		defer func(conn net.Conn) {
-			_ = conn.Close()
-		}(conn)
+		defer func(client net.Conn) { _ = client.Close() }(client)
+		defer func(conn net.Conn) { _ = conn.Close() }(conn)
 		_, _ = io.Copy(conn, client)
 	}()
 }
 
-func updateProxyList() {
-	colorfulog.Info("启动代理更新协程")
-	for {
-		updateUpstreamProxy()
-		time.Sleep(time.Second * 10)
-	}
-}
-
 func checkProxyList() {
-	colorfulog.Info("启动代理检测协程")
+	log.Println("启动代理检测协程")
 	for {
-		colorfulog.Info("代理检测中，代理池容量:", proxyList.Length())
-		for i := 0; i < proxyList.Length(); i++ {
-			index := i
-			go func() {
-				valid, timeOut := checkProxy(proxyList.Get(index).(string))
-				if proxyList.Get(index) != nil && (!valid || timeOut > 1) {
-					if val, ok := proxyList.Get(index).(string); ok {
-						invalid <- val
-					}
+		log.Printf("代理检测中，代理池容量: %d\n", len(proxyList)-len(freeIndex))
+		for i := 0; i < len(proxyList) && proxyList[i] != ""; i++ {
+			go func(index int) {
+				valid, _ := checkProxy(proxyList[index])
+				if !valid {
+					log.Printf("remove inactive proxy %s", proxyList[index])
+					mu.Lock()
+					defer mu.Unlock()
+					proxyList[index] = ""
+					freeIndex <- index
 				}
-			}()
+			}(i)
 		}
-		time.Sleep(time.Second * 10)
-	}
-}
-
-func removeProxy() {
-	for proxy := range invalid {
-		clearFunc := func(index int, item interface{}) bool {
-			return item != nil && index == index && item.(string) == proxy
-		}
-		proxyList.ClearTargets(clearFunc)
-		colorfulog.Infof("remove proxy %s", proxy)
+		time.Sleep(time.Second * 7)
 	}
 }
 
 func main() {
+	for i := 0; i < 50; i++ {
+		freeIndex <- i
+	}
 
-	//启动代理更新线程
-	go updateProxyList()
 	go checkProxyList()
-	go removeProxy()
+	go func() {
+		for {
+			addSomeProxies()
+			time.Sleep(10 * time.Second)
+		}
+	}()
 
+	rand.Seed(time.Now().UnixNano())
 	listener, err := net.Listen("tcp", ":12315")
 	if err != nil {
 		log.Fatalf("Failed to setup listener: %v", err)
@@ -178,12 +175,10 @@ func main() {
 			log.Fatalf("ERROR: failed to accept listener: %v", err)
 		}
 		log.Printf("Accepted connection from %v\n", conn.RemoteAddr().String())
-		var px interface{}
-		px = proxyList.RandomGet()
-		if px == nil {
-			//随便设置个无效的代理
+		px := getRandomProxy()
+		if px == "" {
 			px = "127.0.0.1:60001"
 		}
-		go forward(conn, px.(string))
+		go forward(conn, px)
 	}
 }
